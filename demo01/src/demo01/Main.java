7b3a716118f11369145bad8dc0a2619ffa2adb54
Cristian Sanchez <csanchezptec@gmail.com>
2014-06-09 11:04:50 -0500

    reinicializando archivos

---
+++ demo01/src/demo01/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package demo01;
+
+/**
+ * @author cristian
+ * Mi Primer proyecto
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Mensaje Confirmado");
+	}
+
+}
